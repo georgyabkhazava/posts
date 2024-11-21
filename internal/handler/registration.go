5e143be934f87883c8b6f90e56042df5399b1e7e
georgyabkhazava <know26how@gmail.com>
2024-11-21 15:10:51 +0300

    Добавил валидацию пароля

--- internal/handler/registration.go
+++ internal/handler/registration.go
@@ -3,11 +3,24 @@ package handler
 import (
 	"fmt"
 	"github.com/gin-gonic/gin"
+	"unicode"
 )
 
 type RegistrationRequest struct {
 	Name     string `json:"name"`
 	Password string `json:"password"`
+}
+
+func (r RegistrationRequest) isValidatePassword() bool {
+	if len(r.Password) < 8 {
+		return false
+	}
+	for _, p := range r.Password {
+		if unicode.IsUpper(p) {
+			return true
+		}
+	}
+	return false
 }
 
 func (h *Handler) HandleRegistration(c *gin.Context) {
@@ -16,6 +29,13 @@ func (h *Handler) HandleRegistration(c *gin.Context) {
 	if err != nil {
 		c.JSON(400, gin.H{
 			"message": err.Error(),
+		})
+		return
+	}
+
+	if request.isValidatePassword() == false {
+		c.JSON(400, gin.H{
+			"message": "Not Validate",
 		})
 		return
 	}
