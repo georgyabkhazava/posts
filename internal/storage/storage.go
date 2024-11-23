1997e5d9d7d40c4cfe70b39e90a0a16be8e0cb95
Valeriy Totubalin <vstotubalin@avito.ru>
2024-11-23 13:22:15 +0300

    GetUserID

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -3,6 +3,7 @@ package storage
 import (
 	"context"
 	"database/sql"
+	"github.com/georgyabkhazava/posts/internal/domain"
 	"github.com/pkg/errors"
 )
 
@@ -23,4 +24,22 @@ func (s *Storage) SaveUser(ctx context.Context, name string, passwordHash string
 	}
 
 	return id, nil
+}
+
+// GetUserID Возвращает id пользователя по имени и хэшу пароля
+func (s *Storage) GetUserID(ctx context.Context, name string, passwordHash string) (int64, error) {
+	query := `select id from users where name = $1 and password = $2 limit 1;`
+
+	var userID int64
+	row := s.db.QueryRowContext(ctx, query, name, passwordHash)
+	err := row.Scan(&userID)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return 0, domain.ErrUserNotFound
+		}
+
+		return 0, err
+	}
+
+	return userID, nil
 }
